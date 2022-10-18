d99e4c80dee8945485da2435471446ab17ce6f15
volgaApps <volgaapps@gmail.com>
2022-10-18 21:24:07 +0300

    ллл

--- games/cross_road/App.js
+++ games/cross_road/App.js
@@ -1,7 +1,9 @@
 import {Component} from 'react';
 import {withRouter} from 'react-router';
+
 class App extends Component {
   componentDidMount() {
+    console.log("App init");
     this.props.router.setRouteLeaveHook(this.props.route, this.onLeave);
   }
   onLeave(nextState) {
