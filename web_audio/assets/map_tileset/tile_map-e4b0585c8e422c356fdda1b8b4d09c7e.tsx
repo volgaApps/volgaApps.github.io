563c125105eb4c296f7c076b1876b3c7bcd2a668
rusforapps@gmail.com <rusforapps@gmail.com>
2023-03-06 19:27:52 +0300

    dsfs

---
+++ web_audio/assets/map_tileset/tile_map-e4b0585c8e422c356fdda1b8b4d09c7e.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tile_map" tilewidth="36" tileheight="36" spacing="2" margin="1" tilecount="676" columns="26">
+ <image source="tile_map.png" width="1024" height="1024"/>
+</tileset>
