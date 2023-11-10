21ea5ab5047c1e459c1a53c5ec8a183a965b3edc
rusforapps@gmail.com <rusforapps@gmail.com>
2023-11-10 21:23:55 +0300

    sdf
    sdf

--- uia/api.php
+++ uia/api.php
@@ -1,10 +1,13 @@
-<?php
-$message = $_POST[‘message’]; Извлекаем присланные данные в переменные.
+<?php 
+
+$message = $_POST['message'];
 $cloudiness = $_POST['cloud_value'];
 $timestamp = $_POST['timestamp'];
-254   Глава 10. Подключение к интернету
 $combined = $message." cloudiness=".$cloudiness." time=".$timestamp."\n";
-$filename = "data.txt"; Определяем имя файла, в который будет выполняться запись.
-file_put_contents($filename, $combined, FILE_APPEND | LOCK_EX); Записываем файл.
+
+$filename = "data.txt";
+file_put_contents($filename, $combined, FILE_APPEND | LOCK_EX);
+
 echo "Logged";
+
 ?>
